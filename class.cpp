7b10d39d33e026ae084ac58a1fe22b19b747d972
hyoyup <hyoyup@hotmail.com>
2015-03-22 14:58:19 -0700

    df

--- class.cpp
+++ class.cpp
@@ -981,19 +981,19 @@ LocalGeo Transformation::operator*(LocalGeo localgeo){
 
  
 public:
-      float kdr;
-      float kdg;
-      float kdb;
-      float ksr;
-      float ksg;
-      float ksb;
-      float kar;
-      float kag;
-      float kab;
-      float krr;
-      float krg;
-      float krb;
-      float p;
+      float kdr=0.;
+      float kdg=0.;
+      float kdb=0.;
+      float ksr=0.;
+      float ksg=0.;
+      float ksb=0.;
+      float kar=0.;
+      float kag=0.;
+      float kab=0.;
+      float krr=0.;
+      float krg=0.;
+      float krb=0.;
+      float p=0.;
       BRDF();
       BRDF(float kdr,float kdg, float kdb, float ksr, float ksg, float ksb, float kar, float kag, float kab, float krr, float krg, float krb, float p);
       void setKa(float r,float g, float b);
--- raytracer.cpp
+++ raytracer.cpp
@@ -10,6 +10,7 @@ public:
       AggregatePrimitive primitives;
       int maxrecursiondepth;
       Point eye;
+      BRDF *brdf;
       RayTracer();
       RayTracer(int maxrecursiondepth, Point eye, AggregatePrimitive primitives, std::vector<Light> lights);
       void trace(Ray& ray, int depth, Color* color);
@@ -25,7 +26,7 @@ RayTracer::RayTracer(int maxrecursiondepth, Point eye, AggregatePrimitive primit
 }
 
 void RayTracer::trace(Ray& ray, int depth, Color* color){
-      BRDF *brdf;
+      
       float thit;
       Intersection in = Intersection();
       /* Find the nearest shape that the ray intersects, if any */
@@ -46,7 +47,7 @@ void RayTracer::trace(Ray& ray, int depth, Color* color){
             
 
 
-      /*
+      
       //loop through lights
       int i = 0;
       for(iter = lights.begin(); iter < lights.end(); iter++, i++) {
@@ -60,20 +61,20 @@ void RayTracer::trace(Ray& ray, int depth, Color* color){
             Vector r = l.reverse().add(n.scalarmultiply(2*NdotL));
             r = r.normalize();
             Vector v = (eye.PsubtractP(in.localGeo.get_pos())).normalize(); 
+            float fmx = fmax(NdotL, 0);
+            Color diffuse_comp = Color(brdf->kdr * lcolor->get_r()*fmx, brdf->kdg*lcolor->get_g()*fmx, brdf->kdb*lcolor->get_b()*fmx);
+            //float rdv = 
+            //Color spec_comp = Color(brdf->ksr*lcolor->get_r()*pow(fmax(r.dot(v),0), brdf->p), brdf->ksg*lcolor->get_g()*pow(fmax(r.dot(v),0), brdf->p), brdf->ksb*lcolor->get_b()*pow(fmax(r.dot(v),0), brdf->p));
 
-            Color diffuse_comp = Color(brdf.kdr * lcolor->get_r()*fmax(NdotL, 0), brdf.kdg*lcolor->get_g()*fmax(NdotL, 0), brdf.kdb*lcolor->get_b()*fmax(NdotL, 0));
+            //Color ambient_comp = Color(brdf->kar*lcolor->get_r(), brdf->kag*lcolor->get_g(), brdf->kab*lcolor->get_b());
 
-            Color spec_comp = Color(brdf.ksr*lcolor->get_r()*pow(fmax(r.dot(v),0), brdf.p), brdf.ksg*lcolor->get_g()*pow(fmax(r.dot(v),0), brdf.p), brdf.ksb*lcolor->get_b()*pow(fmax(r.dot(v),0), brdf.p));
-
-            Color ambient_comp = Color(brdf->kar*lcolor->get_r(), brdf->kag*lcolor->get_g(), brdf->kab*lcolor->get_b());
-
-            *color = *color + diffuse_comp + spec_comp + ambient_comp;
+            //*color = *color + diffuse_comp + spec_comp + ambient_comp;
 
         }else{
             *color = *color + Color(brdf->kar*lcolor->get_r(), brdf->kag*lcolor->get_g(), brdf->kab*lcolor->get_b());
         }
     }
-      if(brdf.krr > 0 || brdf.krg > 0 || brdf.krb > 0){
+     /* if(brdf->krr > 0 || brdf->krg > 0 || brdf->krb > 0){
         
 
         Vector n = in.localGeo.normal;
@@ -86,6 +87,6 @@ void RayTracer::trace(Ray& ray, int depth, Color* color){
         Color temp = Color(0,0,0);
         trace(reflectRay, depth+1, &temp);
         *color = *color + Color(brdf->krr*temp.get_r(), brdf->krg*temp.get_g(), brdf->krb*temp.get_b());
-    }
-*/
+    }*/
+
 }
